3f0df4e4d5adbde91485bf8f71d3527d653d0ef5
mutiba-akram <mutibaakram127@gmail.com>
2023-04-16 10:26:42 -0700

    assignment 1

--- scripts/app.js
+++ scripts/app.js
@@ -42,103 +42,103 @@
 // alert( "Your Zakat value is" + result);
 
 
-//                                           // Assignment 1
-//              // Task no 1
-// // Q: Calculate the Zakat value//
-// var zakat_Percentage = 0.025;
-// var user_Input = +prompt( "Enter your total wealth of zakat calculation: " );
-// // user_Input = parseInt(user_Input);
-// console.log( typeof Number (user_Input));
-// var result = zakat_Percentage * user_Input;
-// alert( "Your Zakat value is" + result);
+                                          // Assignment 1
+             // Task no 1
+// Q: Calculate the Zakat value//
+var zakat_Percentage = 0.025;
+var user_Input = +prompt( "Enter your total wealth of zakat calculation: " );
+// user_Input = parseInt(user_Input);
+console.log( typeof Number (user_Input));
+var result = zakat_Percentage * user_Input;
+alert( "Your Zakat value is" + result);
 
 
-//             //  Task no 2
-// // Q: Calculate the Fitrah amount//
-// var familyMembers = parseInt(prompt("Please enter the total number of family members:"));
-// var method = prompt("Please choose a fitrah method: \n 1. 250Rs per family member \n 2. 450Rs per family member \n 3. 2100Rs per family member \n 4. 2800Rs per family");
-// var price;
+            //  Task no 2
+// Q: Calculate the Fitrah amount//
+var familyMembers = parseInt(prompt("Please enter the total number of family members:"));
+var method = prompt("Please choose a fitrah method: \n 1. 250Rs per family member \n 2. 450Rs per family member \n 3. 2100Rs per family member \n 4. 2800Rs per family");
+var price;
 
-// if (method === "1") {
-//   price = 20;
-// }
-//  else if (method === "2") {
-//   price = 25;
-// }
-//  else if (method === "3") {
-//     price = 30
-//  }
-//  else if (method === "4") {
-//     price = 35
-//  }
-// else {
-//   price = parseFloat(prompt("Please enter the custom amount per family member:"));
-// }
-
-
-// var fitrahAmount = price * familyMembers;
-// alert("The calculated fitrah amount for " + familyMembers + " family members is Rs" + fitrahAmount.toFixed(2) + ".");
+if (method === "1") {
+  price = 20;
+}
+ else if (method === "2") {
+  price = 25;
+}
+ else if (method === "3") {
+    price = 30
+ }
+ else if (method === "4") {
+    price = 35
+ }
+else {
+  price = parseFloat(prompt("Please enter the custom amount per family member:"));
+}
 
 
-//               // Task no 3    
-// // Generate a random number between 1 and 10 and store it in a variable called "secretNumber"
-// var secretNumber = 9
-// console.log(secretNumber)
-// var guess = parseInt(prompt("Guess the secret number (between 1 and 10):"));
+var fitrahAmount = price * familyMembers;
+alert("The calculated fitrah amount for " + familyMembers + " family members is Rs" + fitrahAmount.toFixed(2) + ".");
 
 
-// if (guess === secretNumber) {
-//   alert("Congratulations! You guessed the secret number.");
-// }
-
-//  else {
-//   if (guess > secretNumber) {
-//     alert("Too high! Guess again.");
-//   } else {
-//     alert("Too low! Guess again.");
-//   }
-// }
+              // Task no 3    
+// Generate a random number between 1 and 10 and store it in a variable called "secretNumber"
+var secretNumber = 9
+console.log(secretNumber)
+var guess = parseInt(prompt("Guess the secret number (between 1 and 10):"));
 
 
-//             //   Task no 4
-// // Q: Create a program that asks the user to enter a name, and then prints out the name
-// // with the first letter capitalized (Make your name in capitalized case).    
+if (guess === secretNumber) {
+  alert("Congratulations! You guessed the secret number.");
+}
 
-// var name = prompt("Please enter your name:");
-// var capitalized = name.charAt(0).toUpperCase() + name.slice(1);
-// console.log(capitalized);
+ else {
+  if (guess > secretNumber) {
+    alert("Too high! Guess again.");
+  } else {
+    alert("Too low! Guess again.");
+  }
+}
 
 
-           // Task no 5 
-// // Q:Creating two empty arrays called "contactNumbers" and"contactNames"
-// var contactNames =[];
-// var contactNumbers =[];
-// for( i=0;i<3;i++)
-// {
-// var x=prompt("Enter your usename");
-// var y=prompt("Enter your number");
-// contactNames.push(x);
-// contactNumbers.push(y);
-// }
+            //   Task no 4
+// Q: Create a program that asks the user to enter a name, and then prints out the name
+// with the first letter capitalized (Make your name in capitalized case).    
 
-// for(j=0;j<3;j++)
-// {
-//   var name2=contactNames[j];
-//   var number2=contactNumbers[j];
-//   console.log(name2+number2);
-// }
+var name = prompt("Please enter your name:");
+var capitalized = name.charAt(0).toUpperCase() + name.slice(1);
+console.log(capitalized);
+
+
+//            Task no 5 
+// Q:Creating two empty arrays called "contactNumbers" and"contactNames"
+var contactNames =[];
+var contactNumbers =[];
+for( i=0;i<3;i++)
+{
+var x=prompt("Enter your usename");
+var y=prompt("Enter your number");
+contactNames.push(x);
+contactNumbers.push(y);
+}
+
+for(j=0;j<3;j++)
+{
+  var name2=contactNames[j];
+  var number2=contactNumbers[j];
+  console.log(name2+number2);
+}
 
 //              //   Task no 6             
-// // Q: Create an array containing different products
-// var products = ["Apple", "Banana", "Orange", "Grapes", "Mango"];
-// var position = parseInt(prompt("Enter the position of the item to be removed (0 to " + (products.length - 1) + "):"));
-// var removedItem = products.splice(position, 1);
-// console.log("Removed item: " + removedItem);
-// console.log("Remaining items: " + products);
-// console.log("Total number of items remaining: " + products.length);
+// Q: Create an array containing different products
+var products = ["Apple", "Banana", "Orange", "Grapes", "Mango"];
+var position = parseInt(prompt("Enter the position of the item to be removed (0 to " + (products.length - 1) + "):"));
+var removedItem = products.splice(position, 1);
+console.log("Removed item: " + removedItem);
+console.log("Remaining items: " + products);
+console.log("Total number of items remaining: " + products.length);
 
 
-            //   Task no 7
+//               Task no 7
 // Q:Create a program that asks the user for their nationality, gender, and age using the prompt
 // function.            
 var nationality = prompt("What is your nationality?");
@@ -168,13 +168,13 @@ else {
 
 
 //                 //  Task no 8
-// // Q:// Define the WorldCupSquad array
-// const WorldCupSquad = ["Babar Azam", "Mohammad Rizwan", "Shoaib Malik", "Asif Ali", "Mohammad Hafeez", "Fakhar Zaman", "Imad Wasim", "Shadab Khan", "Hasan Ali", "Shaheen Afridi", "Haris Rauf", "Sarfraz Ahmed", "Faheem Ashraf", "Mohammad Hasnain", "Usman Qadir"];
-// const FinalTeam = WorldCupSquad.slice(0, 11);
-// console.log("Final team players:");
-// for (let i = 0; i < FinalTeam.length; i++) {
-//   console.log(FinalTeam[i]);
-// }
+// Q:// Define the WorldCupSquad array
+const WorldCupSquad = ["Babar Azam", "Mohammad Rizwan", "Shoaib Malik", "Asif Ali", "Mohammad Hafeez", "Fakhar Zaman", "Imad Wasim", "Shadab Khan", "Hasan Ali", "Shaheen Afridi", "Haris Rauf", "Sarfraz Ahmed", "Faheem Ashraf", "Mohammad Hasnain", "Usman Qadir"];
+const FinalTeam = WorldCupSquad.slice(0, 11);
+console.log("Final team players:");
+for (let i = 0; i < FinalTeam.length; i++) {
+  console.log(FinalTeam[i]);
+}
 
 
                
